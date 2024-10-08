6b5856a2e0c52212d75f8ccbfe6fb615d3a1e454
David Campuzano <dcampuzano101@gmail.com>
2024-10-08 17:11:26 -0400

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+// eslint-disable-next-line @typescript-eslint/no-var-requires, @typescript-eslint/no-unused-vars
+import path from 'path';
+const nextConfig = {
+    webpack: (config) => {
+        // Add custom rules for binary files and .node files
+        config.module.rules.push(
+            {
+                test: /\.bin$/, // Adjust this regex if necessary for your binary files
+                use: 'binary-base64-loader',
+            },
+            {
+                test: /\.node$/,
+                loader: 'node-loader',
+            }
+        );
+
+        return config;
+    },
+};
 
 export default nextConfig;
